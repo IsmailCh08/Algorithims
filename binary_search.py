31ec20625f60d9407a07b39e168e601d62c38f00
Ismail <Ismailchaudry123@icloud.com>
2026-05-25 20:02:11 -0500

    BInary search done

--- binary_search.py
+++ binary_search.py
@@ -2,13 +2,16 @@
 def binary_Search(arr, number):
     low = 0
     high = len(arr) - 1
-    guess = high // 2
     while low <= high:
-        if number == arr[guess]:
-            print('{arr[guess]} is your number ')
-        elif number > arr[guess]:
-            high += 1
-        elif number < arr[guess]:
-            low += 1
+        mid = (high+low) // 2
+        if number == arr[mid]:
+            return arr[mid]
+        elif number > arr[mid]:
+            low = mid + 1
+        elif number < arr[mid]:
+            high = mid - 1 
         else:
             print('Your number is not in the array')
+    return None
+
+print(binary_Search([1,2,3,5,8,7,9], 1))
